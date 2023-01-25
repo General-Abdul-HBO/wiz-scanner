6852b81e6974898ac96fed5ad6f8a22c9939ccdf
rcarpio-hbo <rosa.carpio@warnermedia.com>
2023-01-25 11:54:33 +0100

    Changes done: - Remove unnecessary code - Change get_vulnerabilities function to make it work with the new json - new function called set_report_title to generate the title
    Missing:
    - Format Risk factors data if you want to show it
    - test the script at least  with two more json: without any vulnerability (alpine:3.17) and with an image with fixed vuln

--- prismacloud/json2html.py
+++ prismacloud/json2html.py
@@ -8,120 +8,79 @@ import argparse
 # commented out the secrets line of code prisma doesnt scan for secrets i guess.
 # changes are a bit messy while trying to figure it out.
 
-# prismacloud vulnerabilities types
-vuln_types = ["packages","applications","compliances","complianceDistribution","complianceScanPassed","vulnerabilities","vulnerabilityDistribution","vulnerabilityScanPassed","history","scanTime","scanID"]
-
 def read_json(path: str):
     f = open(path)
     data = json.load(f)
     f.close()
     return data
 
-# def merge_vuln_secrets (vuln: list, secrets: list):
-
-#     # If there are vuln and secrets, merge both lists
-#     if vuln == [] and secrets == None:
-#         print("INFO: Lists can not be merged:\n" + 
-#               "       + vuln: " + str(type(vuln)) + "\n" +
-#               "       + secrets: " + str(type(secrets)))
-#         return None
-#     else:
-#         if vuln == None:
-#             table_data = secrets
-#         elif secrets == None:
-#             table_data = vuln
-
-#         else:
-#             table_data = vuln
-
-#             for item in secrets:
-#                 table_data.append(item)
-
-    
-#     return table_data
-
-def get_vulnerabilities_by_type (data: dict, type: str):
-    if data.get(type) is None:
-        print("We do not have " + type + " vulnerabilities")
-        return None
-    else:
-        vulnerabilities = []
-      
-        for item in data.get(type):
-            # Path value!!  not sure if i need a path.
-            if item.get('path') == None:
-                path = '-'
-            else: 
-                path = item.get('path')
-
-            for vuln in item.get('vulnerabilities'):
-                # Fixed Version value
-                if vuln.get('status') == None:
-                    fixedStatus = '-'
-                else: 
-                    fixedStatus = vuln.get('status')
-
-            # i did this first to get type located in packages not sure if this is the right way or the second way is better.
-            # for package in item.get('packages'):
-            #     if package.get('type') == None:
-            #         type = '-'
-            #     else:
-            #         type = package.get('type')
-                data_vuln = {
-                    # 'Type': type,
-                    'Type': data.get("packages").get("type"),
-                    'Severity': vuln.get('severity'),
-                    'Name': vuln.get('packageName'),
-                    'Version': vuln.get('packageVersion'),
-                    'CVE': vuln.get('id'),
-                    'CVSS': vuln.get('cvss'),
-                    'Fixed Status': fixedStatus,
-                    
-                    'Link': '<a href="' + vuln.get('link') + '">More information</a>'
-                }
-                vulnerabilities.append(data_vuln)
-
-        return vulnerabilities
-
 def get_vulnerabilities (data: dict):
-    vulnerabilities=[]
-
-
-    for vuln_type in vuln_types:
-        vuln = get_vulnerabilities_by_type (data, vuln_type)
-        if vuln is not None:
-            for item in vuln:
-                vulnerabilities.append(item)
+    vulnerabilities = []
+    # Iterate thought the results list
+    for item in data.get("results"):
+        vulns_json = item.get("vulnerabilities")
+        packages_json = item.get("packages")
         
+        for vuln in vulns_json:
+
+            data_vuln = {
+                'Type': next((pkg.get("type") for pkg in packages_json if pkg['name'] == vuln.get('packageName')), None),
+                'Severity': vuln.get('severity').upper(),
+                'Package': vuln.get('packageName'),
+                # 'Version': vuln.get('packageVersion'),
+                'CVE': '<a href="' + vuln.get('link') + '">' + vuln.get('id') + '</a>',
+                # 'Fixed Status': fixedStatus,
+                # 'Risk factors': next((risk for risk in vuln.get('riskFactors') if vuln.get('riskFactors') != None ), None),
+                'Risk factors': '-' if vuln.get('riskFactors') is None else " ".join(vuln.get('riskFactors')),
+                'Description': vuln.get('description'),
+            }
+            vulnerabilities.append(data_vuln)
+
+
+    # if data.get(type) is None:
+    #     print("We do not have " + type + " vulnerabilities")
+    #     return None
+    # else:
+    #     vulnerabilities = []
+      
+    #     for item in data.get(type):
+    #         # Path value!!  not sure if i need a path.
+    #         if item.get('path') == None:
+    #             path = '-'
+    #         else: 
+    #             path = item.get('path')
+
+    #         for vuln in item.get('vulnerabilities'):
+    #             # Fixed Version value
+    #             if vuln.get('status') == None:
+    #                 fixedStatus = '-'
+    #             else: 
+    #                 fixedStatus = vuln.get('status')
+
+    #         # i did this first to get type located in packages not sure if this is the right way or the second way is better.
+    #         # for package in item.get('packages'):
+    #         #     if package.get('type') == None:
+    #         #         type = '-'
+    #         #     else:
+    #         #         type = package.get('type')
+    #             data_vuln = {
+    #                 # 'Type': type,
+    #                 'Type': data.get("packages").get("type"),
+    #                 'Severity': vuln.get('severity'),
+    #                 'Name': vuln.get('packageName'),
+    #                 'Version': vuln.get('packageVersion'),
+    #                 'CVE': vuln.get('id'),
+    #                 'CVSS': vuln.get('cvss'),
+    #                 'Fixed Status': fixedStatus,
+                    
+    #                 'Link': '<a href="' + vuln.get('link') + '">More information</a>'
+    #             }
+    #             vulnerabilities.append(data_vuln)
+
     return vulnerabilities
 
-# def get_secrets (data: dict):
-#     secrets = []
-
-#     if data.get("secrets") is None:
-#         print("We do not have secrets")
-#         return None
-#     else:
-#         for item in data.get("secrets"):
-#                 data_secret = {
-#                     'Type': 'secret',
-#                     'Name': item.get('description'), 
-#                     'Path': item.get('path'), 
-#                     'Version': '-',
-#                     'Fixed Version': 'Remove secret from container',
-#                     'CVE': '-',
-#                     'Severity': '-',
-#                     'Source': '-'
-#                 }
-#                 secrets.append(data_secret)
-#     return secrets
-
 def generate_table_figure(data:dict, table_data: list, output_file: str):
-    subtitle_result = set_report_result(data.get("vulnerabilityScanPassed"))
-    container_image = data.get("results").get("name")
-    distro = data.get("results").get("distro")
-
-    layout_title = "<b>Prismacloud Scanning - Image: " + container_image + "</b><br>" +  "<br>" + subtitle_result + "<br>" + "<b>Distro:</b> " + distro + "<br>"
+    layout_title = set_report_title (data)
 
     df = pd.DataFrame.from_dict(table_data)
 
@@ -164,16 +123,25 @@ def generate_table_figure(data:dict, table_data: list, output_file: str):
     
     offline.plot(fig, filename = output_file, auto_open=False, show_link=False)
 
-def set_report_result (scanning_status: str):
-    if "false" in scanning_status:
-        subtitle_result = "<b>Scan result:</b> <span style='color:red'>FAILED</span>"
-        # Scan results: FAILED. Container image does not meet policy requirements
+def set_report_title (data:dict):
+    title = "<b>Prismacloud Scanning</b><br>"
 
-    if "true" in scanning_status:
-        subtitle_result = "<b>Scan result:</b> <span style='color:green'>PASSED</span>"
+    container_images = [image.get("name") for image in data.get("results")]
+    scan_results = [image.get("vulnerabilityScanPassed") for image in data.get("results")]
 
-    return subtitle_result
+    for (image, result) in zip(container_images, scan_results):
+        print (image, result)
+        print("---")
 
+        title = title + "<b> - Image: " + image + "</b>, "
+
+        if result is False:
+            title = title + "<b>Scan result: </b><span style='color:red'>FAILED</span>. Container image does not meet policy requirements<br>"
+
+        if result is True:
+            title = title + "<b>Scan result: </b><span style='color:green'>PASSED</span>. Container image meets policy requirements<br>"
+
+    return title
 
 def parse_arguments():
     argParser = argparse.ArgumentParser()
@@ -189,14 +157,14 @@ def main():
     input_file,output_file = parse_arguments()
 
     json_data = read_json(input_file)
-    vuln = get_vulnerabilities (json_data.get("results"))
+    vulns = get_vulnerabilities (json_data)
     
 
     # if vuln == [] and secrets == None:
     #     print("\nCongratulations!!! The container image does not have vulnerabilities")
     # else:
     #     table_data = merge_vuln_secrets (vuln, secrets)
-    generate_table_figure( json_data,  output_file )
+    generate_table_figure( json_data, vulns,  output_file )
 
 if __name__ == '__main__':
     main()
